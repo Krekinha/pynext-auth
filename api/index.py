00a5645461feb66ebd73e59a9995ed81f1008d85
Krekinha <krekmg@gmail.com>
2024-07-13 16:36:50 -0300

    update with python scrap and fastapi api

--- api/index.py
+++ api/index.py
@@ -1,15 +1,59 @@
-from flask import Flask, make_response, jsonify
+from fastapi import FastAPI
+from selenium import (
+    webdriver,  # irá lidar com o download da melhor versão para o chrome
+)
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
 
-app = Flask(__name__)
+# from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+app = FastAPI()
 
 
-@app.route('/flask', methods=['GET'])
+@app.get("/api/python")
 def hello_world():
-    res = 'API FLASK: ONLINE'
-    return make_response(jsonify(res))
+    try:
+        scrap()
+        return {"message": "Scrap success!"}
+    except Exception as error:
+        return str(error)
 
 
-if __name__ == '__main__':
-    # app.run(port=8000)
-    from waitress import serve
-    serve(app, host="127.0.0.1", port=8000)
+def scrap():
+
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+
+    # irá identificar a versão do chrome instalada e instalara o ChromeDriver correspondente
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=chrome_options,
+    )
+
+    driver.get(
+        "https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M"
+    )
+
+    el_nome = driver.find_element(
+        "xpath",
+        '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input',
+    )
+    el_email = driver.find_element(
+        "xpath",
+        '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input',
+    )
+    el_zap = driver.find_element(
+        "xpath",
+        '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[3]/div/input',
+    )
+    el_button = driver.find_element(
+        "xpath", '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/button'
+    )
+
+    el_nome.send_keys("Krekinha")
+    el_email.send_keys("krek@teste.com")
+    el_zap.send_keys("33991053110")
+    el_button.click()
+
+    driver.quit()
--- next.config.js
+++ next.config.js
@@ -3,12 +3,25 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api-py/:path*", // Para todas as requisições que começam com '/api-py'
-        destination: "http://127.0.0.1:8000/:path*", // redirecionar para o backend
-
-        /*process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:3001/api/:path*"
-            : "/api/:path*",*/
+        source: "/apipy/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/apipy/",
+      },
+      {
+        source: "/docs",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/docs"
+            : "/apipy/docs",
+      },
+      {
+        source: "/openapi.json",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/openapi.json"
+            : "/apipy/openapi.json",
       },
     ];
   },
