d00d6c22ee8e399fd05781f6040640fb10afcf69
Krekinha <krekmg@gmail.com>
2024-07-13 18:54:41 -0300

    18:54

--- src/app/(noMenu)/auth/login/layout.tsx
+++ src/app/(noMenu)/auth/login/layout.tsx
@@ -1,5 +1,4 @@
-import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth/next";
+import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import { redirect } from "next/navigation";
 
 export default async function PrivateLayout({
@@ -10,9 +9,14 @@ export default async function PrivateLayout({
   // Antes de renderiza a página de login, irei verificar
   // se o usuário está logado. Se estiver, irei redirecionar para a
   // a página principal da aplicação
-  const session = await getServerSession(nextAuthOptions);
+  const session = await auth();
+
+  if (!session) {
+    console.log("você não está logado");
+  }
 
   if (session) {
+    console.log("você já está logado");
     redirect("/");
   }
 
--- src/app/api/auth/users/login/route.ts
+++ src/app/api/auth/users/login/route.ts
@@ -1,46 +1,55 @@
 import { prisma } from "@/utils/prisma";
+import { PrismaClientValidationError } from "@prisma/client/runtime/library";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export async function POST(req: Request) {
-  const data = await req.json();
+  try {
+    const data = await req.json();
 
-  console.log("DATA API: ", data);
+    console.log("DATA API: ", data);
 
-  // Verifica se o usuário existe
-  const user = await prisma.user.findUnique({
-    where: {
-      email: data.email,
-    },
-  });
+    // Verifica se o usuário existe
+    const user = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
 
-  console.log("USUÁRIO API: ", user);
+    // Se o usuário não existir, retorna uma mensagem de erro
+    if (!user) {
+      return new Response("Usuário não cadastrado", {
+        status: 401,
+        headers: { erro: "Usuário não cadastrado" },
+      });
+    }
 
-  // Se o usuário não existir, retorna uma mensagem de erro
-  if (!user) {
-    return new Response("Usuário não cadastrado",{status: 401, headers:{"erro":"Usuário não cadastrado"}});
+    // Se o usuário existir, verifica se a senha é válida
+    const isValidPassword = bcrypt.compareSync(data.senha, user.senha);
+
+    // Se a senha for inválida, retorna uma mensagem de erro
+    if (!isValidPassword) {
+      return new Response("Usuário e/ou senha inválidos", {
+        status: 401,
+        headers: { erro: "Usuário e/ou senha inválidos" },
+      });
+    }
+
+    console.log("USUÁRIO API: ", user);
+
+    // Se a senha for válida, usa a chave secreta armazenada no arquivo .env
+    // para gerar um token JWT
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
+      expiresIn: "1d",
+    });
+
+    // Usando a desestruturação para criar um novo objeto "userSemSenha" excluindo a propriedade "senha"
+    // para que não seja retornada na API por questão de segurança
+    const { senha, ...userSemSenha } = user;
+
+    // Retorna o usuário e o token JWT
+    return new Response(JSON.stringify({ user: userSemSenha, token: token }));
+  } catch (error: PrismaClientValidationError | any) {
+    return new Response(error, { status: 500 });
   }
-  
-
-  // Se o usuário existir, verifica se a senha é válida
-  const isValidPassword = bcrypt.compareSync(data.senha, user.senha);
-
-  // Se a senha for inválida, retorna uma mensagem de erro
-  if (!isValidPassword) {
-    return new Response("Usuário ou senha inválida",{status: 401, headers:{"erro":"Usuário ou senha inválida"}});
-  }
-
-  // Se a senha for válida, gera um token JWT
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
-    expiresIn: "1d",
-  });
-
-  // Usando a desestruturação, irei criar um novo objeto "userSemSenha" excluindo a propriedade senha
-  const { senha, ...userSemSenha } = user;
-
-  // Retorna o usuário e o token JWT
-  return Response.json({
-    user: userSemSenha,
-    token: token,
-  });
 }
